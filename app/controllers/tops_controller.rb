e3b760f602dbf3f54ad462631a84019bfc06002e
takesoo <htk.programming@gmail.com>
2019-06-08 21:03:07 +0900

    simple_layout適応記載（エラー）

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -5,6 +5,8 @@ class TopsController < ApplicationController
 
   #sellアクション仮置き
   def sell
+    render :sell, layout: "simple_layout"
+    # sell.html.haml呼び出しの時はlayout/simple_layout.html.hamlを使う
   end
   
 end

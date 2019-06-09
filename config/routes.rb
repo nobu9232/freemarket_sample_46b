4aac330a55cfd2fd2909a13b05e9a0591feb39ec
nobu9232 <nobuyamakunimitsu@nobu9232.local>
2019-06-09 17:39:59 +0900

    ユーザープロフィール編集ページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#index'
-<<<<<<< HEAD
   get 'users/update'
-=======
-  get 'users' => 'users#edit'
   resources :users, action: :show
-  
->>>>>>> origin/ユーザープロフィール編集ページ
 end

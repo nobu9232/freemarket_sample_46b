253e238ec91bb415b845321da3f7b77c1cd35736
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-06-12 19:07:59 +0900

    Userコントローラー編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
 
   def registration
+    render :registration, layout:
+    "user_application"
   end
+
+  def signup
+    render :signup, layout:
+    "user_application"
+  end
+
+
 
   def update
   end
+
   
 
   def show

ae318adbdecc737561336be49ee0e7b2a109d397
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-06-12 23:34:15 +0900

    Loginアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,13 @@ class UsersController < ApplicationController
   def update
   end
   
-
+  def login
+    render :login, layout:
+    "user_application"
+  end
   def show
   end
+  
 
   def sign_out  
   end

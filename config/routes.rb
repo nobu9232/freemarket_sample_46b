19231033d33377640dc85ac8480d27a12a5993d7
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-06-21 22:07:20 +0900

    空白削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#index'
-
   get 'users/signup'
   get 'users/update'
   get 'users/sign_out'
-
   resources :users, action: :registration
 end
 

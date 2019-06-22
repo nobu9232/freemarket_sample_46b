a132a9e43f9da5659a4d732a62b2b40bf9da4f29
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-06-22 13:50:25 +0900

    Merge branch 'master' into 商品購入確認ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#index'
+  resources :tops, except: :index, action: :sell
   get 'users/update'
   get 'users/sign_out'
+<<<<<<< HEAD
   get 'products/confirmation'
   resources :users
+=======
+  resources :users, only: :show
+>>>>>>> master
 end
 

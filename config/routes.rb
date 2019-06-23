648877c63963645c3a08f410b447ab0d7e238b26
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-06-23 23:06:22 +0900

    routes変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#index'
   get 'users/signup'
-  resources :users, action: :registration
+  get 'users/registration'
+  get 'users/login'
+  get 'users/identification'
   resources :tops, except: :index, action: :sell
   get 'users/update'
   get 'users/sign_out'
-<<<<<<< HEAD
-  resources :users, action: :login
-end
-
-=======
   resources :products, action: :confirmation
   get 'users/card'
-  resources :users, only: :show
-  get 'users/identification'
 end
->>>>>>> master

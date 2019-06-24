cb3828813c504e4e1cb36b3340fa416b443d96ba
nobu9232 <nobuyamakunimitsu@nobu9232.local>
2019-06-24 16:40:08 +0900

    コメントアウト削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'tops#index'
-  # get 'users/signup'
-  # resources :users, action: :registration
-  # resources :tops, except: :index, action: :sell
-  # get 'users/update'
-  # get 'users/sign_out'
-  # resources :products, action: :confirmation
-  # get 'users/card'
-  # resources :users, only: :show
-  # get 'users/identification'
-
+  
   root 'products#index'
   resources :products do
     collection do

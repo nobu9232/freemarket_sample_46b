efd09019b086d2439389413651a253ed6d09bee0
nobu9232 <nobuyamakunimitsu@nobu9232.local>
2019-06-24 16:19:33 +0900

    routes.rbをresourcesに修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,34 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'tops#index'
-  get 'users/signup'
-  resources :users, action: :registration
-  resources :tops, except: :index, action: :sell
-  get 'users/update'
-  get 'users/sign_out'
-  resources :products, action: :confirmation
-  get 'users/card'
-  resources :users, only: :show
-  get 'users/identification'
+  # root 'tops#index'
+  # get 'users/signup'
+  # resources :users, action: :registration
+  # resources :tops, except: :index, action: :sell
+  # get 'users/update'
+  # get 'users/sign_out'
+  # resources :products, action: :confirmation
+  # get 'users/card'
+  # resources :users, only: :show
+  # get 'users/identification'
+
+  root 'products#index'
+  resources :products do
+    collection do
+      get 'sell'
+      get 'confirmation'
+    end
+  end
+
+  resources :users do
+    collection do
+      get 'registration'
+      get 'signup'
+      get 'profile'
+      get 'card'
+      get 'sign_out'
+      get 'identification'
+    end
+  end
+  
 end

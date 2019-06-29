7d8b03baffa1af24606bf3bffd8520828556d484
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-06-29 15:19:30 +0900

    Productモデルリレーション追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  belongs_to :saler_user, class_name: "User"
+  belongs_to :buyer_user, class_name: "User"
+  # has_many :categories, through: :products_categories, dependent: :destroy
+  # has_many :products_categories
+  # belongs_to :brand
+  # has_many :product_images, dependent: :destroy
+
+
 end

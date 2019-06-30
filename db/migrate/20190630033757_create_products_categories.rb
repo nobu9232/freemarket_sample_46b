903b100fa6b296c47b402c42ef1f3ede1678ca93
takesoo <htk.programming@gmail.com>
2019-06-30 14:50:08 +0900

    Create 20190630033757_create_products_categories.rb

---
+++ db/migrate/20190630033757_create_products_categories.rb
@@ -0,0 +1,10 @@
+class CreateProductsCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products_categories do |t|
+      t.integer :product_id, null:false, foreign_key: true
+      t.integer :category_id, null:false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

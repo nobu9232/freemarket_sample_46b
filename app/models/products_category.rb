219ba6ce5a997e5940bef69446237631ec8ea4fb
takesoo <htk.programming@gmail.com>
2019-07-03 21:46:54 +0900

    category-productアソシエーション設定

--- app/models/products_category.rb
+++ app/models/products_category.rb
@@ -1,6 +1,6 @@
 class ProductsCategory < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
+
+  belongs_to :category
   belongs_to :product
 
 end

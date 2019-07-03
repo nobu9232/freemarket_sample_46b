27913a80de5a67f0a3b50a2a6d2f39ee59969f0b
takesoo <htk.programming@gmail.com>
2019-07-03 20:28:32 +0900

    商品の状態enum導入

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,14 +3,17 @@ class Product < ApplicationRecord
   belongs_to :saler_user, class_name: "User"
   belongs_to :buyer_user, class_name: "User"
 
-  # 商品の状態enum
+  # 取引段階enum
   enum status: { "出品中":0, "取引中":1, "落札済":2, "発送中":3, "取引終了":4}
 
   # 発送日数enum
   enum shipping_days: { "---":0, "1~2日で発送":1, "2~3日で発送":2, "4~7日で発送":3}
 
-  # 発送料負担enum
+  # 配送料負担enum
   enum shipping_charge: { "---":0, "送料込み（出品者負担）":1, "着払い（購入者負担）":2}, _suffix: true
+
+  # 商品の状態enum
+  enum condition: { "---":0, "新品。未使用":1, "未使用に近い":2, "目立った傷や汚れなし":3, "やや傷や汚れあり":4, "傷や汚れあり":5, "全体的に状態が悪い":6}, _suffix: true
 
   # カテゴリーとのアソシエーション
   has_many :categories, through: :products_categories, dependent: :destroy

3fff43729fccb339f9c72dcf959cd2750e1f561f
nobu9232 <nobuyamakunimitsu@nobu9232.local>
2019-07-07 15:28:38 +0900

    アソシエーション定義

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :saler_user, class_name: "User"
   belongs_to :buyer_user, class_name: "User"
+  has_many   :product_images, class_name: "ProductImage", dependent: :destroy
+  has_many   :categories, through: :products_categories, dependent: :destroy
+  has_many   :products_categories, class_name: "ProductsCategorie"
   enum status: { "draft":0, "exhibition":1, "bid":2, "transferred":3, "sent":4}
-  # has_many :categories, through: :products_categories, dependent: :destroy
-  # has_many :products_categories
   # belongs_to :brand
-  # has_many :product_images, dependent: :destroy
-
 
 end
---
+++ app/models/products_categorie.rb
@@ -0,0 +1,4 @@
+class ProductsCategorie < ApplicationRecord
+  belongs_to :product
+  belongs_to :category
+end

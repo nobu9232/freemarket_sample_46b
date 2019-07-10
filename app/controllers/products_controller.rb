afe7ba51549da262006dd42a374f635d5cbce724
takesoo <htk.programming@gmail.com>
2019-07-10 20:48:12 +0900

    出品機能実装
    ブランド名がbrandsテーブルにすでにあるなら保存しないように設定した

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,14 +6,53 @@ class ProductsController < ApplicationController
   def show
   end
 
-  def sell
-    render :sell, layout: "simple_layout"
+  def new
+    render layout: "simple_layout"
     @product = Product.new
+  end
+
+  def create
+    binding.pry
+    @brand = Brand.find_by(brand_name: brand_params[:brand_name])
+    if @brand == nil
+      @brand = Brand.create(brand_params)
+    end
+    @product_params = product_params.merge(brand_id: @brand[:id])
+    @product = Product.create(@product_params)
+    @image = Image.new(image_params)
+    @image.product_id = @product[:id]
+    @image.save
+    render :index
   end
 
   def confirmation  
     render :confirmation, layout: "simple_layout"
   end
 
+  private 
+  def product_params
+    params.permit(
+      :name, 
+      :text, 
+      :category_id, 
+      :category_child_id, 
+      :category_grondchild_id, 
+      :size_id, 
+      :condition, 
+      :shipping_charge, 
+      :shipping_method_id, 
+      :prefecture_id, 
+      :shipping_days, 
+      :sales_price
+    ).merge(status: 0, seller_user_id: current_user.id)
+  end
+  
+  def image_params
+    params.permit(:image)
+  end
+
+  def brand_params
+    params.permit(:brand_name)
+  end
 
 end

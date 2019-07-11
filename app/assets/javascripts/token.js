071fe7fdf4fae1e37a65a479ec86c8b268166f3f
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-07-11 23:36:29 +0900

    Token.js追記

---
+++ app/assets/javascripts/token.js
@@ -0,0 +1,31 @@
+$(function(){
+  $(document).on('click','.payjp', function(e) {
+    e.preventDefault();
+    Payjp.setPublicKey('pk_test_947d99d606b2fc4591629e0e');
+    var card = {
+      number: parseInt($(".card-number").val()),
+      cvc: parseInt($(".cvc").val()),
+      exp_month: parseInt($("#exp_month").val()),
+      exp_year: parseInt($("#exp_year").val())
+    };
+    console.log(card)
+    Payjp.createToken(card, function(status, response) {
+        var token = response.id;
+        console.log(token)
+        $.ajax({
+          url: '/users/card_create',
+          type: "POST",
+          data: { token: token },
+          dataType: 'json',
+        })
+        .done(function(){
+          //非同期通信成功時の処理
+        })
+        .fail(function(){
+          alert('非同期通信失敗')
+        })
+    });
+  })
+})
+
+

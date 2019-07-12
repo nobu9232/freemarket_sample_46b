0b87be038f37e4fdf755c1bd43e2823e448816ec
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-07-13 00:35:26 +0900

    If分岐追記
    i

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,18 +21,20 @@ class UsersController < ApplicationController
       format.json {
         require 'payjp'
         Payjp.api_key = "sk_test_47f2b808edb7bc79cf692300"
-        response_customer = Payjp::Customer.create(card: params[:token])
         # 今回は１ユーザー１カード情報のみとする
-        current_user.cards.create(customer_id: response_customer.id,user_id: current_user.id) if current_user.cards ==[]
-        current_user.cards.update(customer_id: response_customer.id,user_id: current_user.id) unless current_user.cards ==[]
+        if current_user.cards ==[]
+          response_customer = Payjp::Customer.create(card: params[:token])
+          current_user.cards.create(customer_id: response_customer.id,user_id: current_user.id) 
+        end
       }
     end
   end
 
   def pay
+    @product = Product.find(1)
     Payjp.api_key = "sk_test_47f2b808edb7bc79cf692300"
     charge = Payjp::Charge.create(
-      amount: 3000,
+      amount: @product.sales_price,
       customer: current_user.cards.first.customer_id,
       currency: 'jpy',
     )

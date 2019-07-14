b9c8647f81158b561160ac8433cd407a2dd46552
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-07-14 17:54:42 +0900

    routeパス修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: 'users/registrations'},controllers: {sessions: 'users/sessions'}
+  devise_for :users, controllers: {registrations: 'users/registrations', sessions: 'users/sessions'}
   
   root 'products#index'
   resources :products do

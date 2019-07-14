a27237b37b44709d72ef1294fee3656cd099c72d
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-07-14 17:32:55 +0900

    product member下に post 'pay'を追記

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
       get 'show_sell'
       get 'confirmation'
       get 'buy'
+      post 'pay'
+
     end
   end
 
@@ -15,7 +17,6 @@ Rails.application.routes.draw do
     collection do
       get 'card_form'
       post 'card_create'
-      post 'pay'
       get 'signup'
       get 'profile'
       get 'card'

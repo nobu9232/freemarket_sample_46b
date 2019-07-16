0b291678b4ffd5da861820b5c343b9e549a4e57b
takesoo <htk.programming@gmail.com>
2019-07-16 21:35:28 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: 'users/registrations', sessions: 'users/sessions'}
+  devise_for :users, controllers: {
+    registrations: 'users/registrations', 
+    sessions: 'users/sessions', 
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   
   root 'products#index'
   resources :products do

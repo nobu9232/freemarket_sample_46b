5c8a32e0845dc71b983e62f69268551202433d17
takesoo <htk.programming@gmail.com>
2019-07-16 21:29:00 +0900

    userモデルにfindメソッド追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
-
+  def self.find_for_oauth(auth)
+    sns = SnsCredential.where(uid: auth.uid, provider: auth.provider).first
+    unless sns
+      @user = User.create(
+      email:    auth.info.email,
+      password: Devise.friendly_token[0,20]
+      )
+      sns = SnsCredential.create(
+      user_id: @user.id,
+      uid: auth.uid,
+      provider: auth.provider
+      )
+    end
+    sns
+    @user
+  end
  
 end

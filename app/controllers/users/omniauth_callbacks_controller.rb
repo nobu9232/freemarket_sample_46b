894b36e10deb495d4635444252cfda51103ea2e1
takesoo <htk.programming@gmail.com>
2019-07-20 16:18:47 +0900

    facebookサインアップ

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,54 +1,27 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
-
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
-
   def facebook
-    callback_from :facebook
+    callback_for(:facebook) #コールバック
   end
 
-  def google_oauth2
-    callback_from :google
-  end
 
-  private
-
-  def callback_from(provider)
-    provider = provider.to_s
-
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-
-    if @user.persisted?
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+  def callback_for(provider)
+    info = User.find_oauth(request.env["omniauth.auth"]) #usersモデルのメソッド
+    @user = info[:user]
+    @user_detail = @user.build_user_detail
+    sns_id = info[:sns_id]
+    # binding.pry
+    if @user.persisted? #userが存在したら
       sign_in_and_redirect @user, event: :authentication
-    else
-      session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
+      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+    else #userが存在しなかったら
+      session["devise.sns_id"] = sns_id #sns_credentialのid devise.他のアクションに持ち越せる(少し難)
+      render template: "devise/registrations/new", layout: "sign_up_layout" #redirect_to だと更新してしまうのでrenderで
     end
+  end
+
+  def failure
+    redirect_to root_path and return
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,23 +9,50 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[facebook google_oauth2]
+         :omniauthable, omniauth_providers: [:facebook]
 
-  def self.find_for_oauth(auth)
-    sns = SnsCredential.where(uid: auth.uid, provider: auth.provider).first
-    unless sns
-      @user = User.create(
-      email:    auth.info.email,
-      password: Devise.friendly_token[0,20]
-      )
-      sns = SnsCredential.create(
-      user_id: @user.id,
-      uid: auth.uid,
-      provider: auth.provider
-      )
+  def self.find_oauth(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    # binding.pry
+
+    if snscredential.present? #sns登録のみ完了してるユーザー
+      user = User.where(id: snscredential.user_id).first
+      unless user.present? #ユーザーが存在しないなら
+        user = User.new(
+          # snsの情報
+          # binding.pry => auth.infoとかで確認 
+          nickname: auth.info.name,
+          email: auth.info.email
+        )
+      end
+      sns = snscredential
+      #binding.pry
+
+    else #sns登録 未
+      user = User.where(email: auth.info.email).first
+      if user.present? #会員登録 済
+        sns = SnsCredential.new(
+          uid: uid,
+          provider: provider,
+          user_id: user.id
+        )
+      else #会員登録 未
+        user = User.new(
+          nickname: auth.info.name,
+          email: auth.info.email
+        )
+        # binding.pry
+        sns = SnsCredential.create(
+          uid: uid,
+          provider: provider
+        )
+        # binding.pry 
+      end
     end
-    sns
-    @user
+    # binding.pry
+    # hashでsnsのidを返り値として保持しておく
+    return { user: user , sns_id: sns.id }
   end
- 
 end

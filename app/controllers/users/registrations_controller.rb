e17ef1d87d6efb2f232f63461de18a02a00e13f2
takesoo <htk.programming@gmail.com>
2019-07-20 22:00:22 +0900

    条件式を訂正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -14,11 +14,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   def create
       #binding.pry
-    if params[:user][:password] == "" #sns登録なら
-      params[:user][:password] = "Devise.friendly_token.first(6)" #deviseのパスワード自動生成機能を使用
-      params[:user][:password_confirmation] = "Devise.friendly_token.first(6)"
+    if session["devise.sns_id"] #sns登録なら
+      params[:user][:password] = Devise.friendly_token.first(6) #deviseのパスワード自動生成機能を使用
+      params[:user][:password_confirmation] = Devise.friendly_token.first(6)
       super
-      # binding.pry
+      binding.pry
       sns = SnsCredential.update(user_id:  @user.id)
     else #email登録なら
       # binding.pry

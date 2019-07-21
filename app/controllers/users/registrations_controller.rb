ca67046b19d2f515623bf2b229f45a6cb9bd7a59
takesoo <htk.programming@gmail.com>
2019-07-21 10:23:37 +0900

    パスワードエラー修正
    passwordとpassword_confilmationが同じ値になるようにした

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -16,9 +16,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
       #binding.pry
     if session["devise.sns_id"] #sns登録なら
       params[:user][:password] = Devise.friendly_token.first(6) #deviseのパスワード自動生成機能を使用
-      params[:user][:password_confirmation] = Devise.friendly_token.first(6)
+      params[:user][:password_confirmation] = params[:user][:password]
       super
-      binding.pry
       sns = SnsCredential.update(user_id:  @user.id)
     else #email登録なら
       # binding.pry
